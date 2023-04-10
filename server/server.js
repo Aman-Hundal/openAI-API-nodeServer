6d41e830412a05f22fd857f935b1a847a7d3e021
Aman Hundal <amanhundal19@gmail.com>
2023-04-09 21:40:44 -0700

    set up server and open ai api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
-const http = require("http")
+const http = require("http");
+const openAI = require("openai");
+
+//opeAi methods - SEE DOCUMENTATION
+const openAIAPI = new openAI.OpenAIApi(
+  new openAI.Configuration({
+    apiKey: process.env.API_KEY,
+  })
+);
 
 // Creating server
 const server = http.createServer((req, res) => {
-	res.write("Hello World")
-	res.end();
-})
+  res.write("Hello World");
+  res.end();
+});
 
-server.listen((3000), () => {
-	console.log("Server is running on PORT 3000");
-})
+server.listen(process.env.PORT, () => {
+  console.log("Server is running on PORT 3000");
+});
